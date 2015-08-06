7247e8db241c23566c5294c2980af7cf5f58d2b6
Goran R <goranradmanovic@gmail.com>
2015-08-06 23:42:10 +0200

    Adding code to index.php and .htaccess file

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+//Ukljucivanje start.php fajla iz app foldera sa svim postavkama za pokretanje aplikacije
+
+require_once 'app/start.php';
+
+//Pokretanje Slim2 aplikacije
+
+$app->run();
+
+?>
