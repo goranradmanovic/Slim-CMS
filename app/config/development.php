9504f4a2f1c29345deeedbfe2b381644d2c93ff7
Goran R <goranradmanovic@gmail.com>
2015-08-08 22:41:48 +0200

    Record all from app folder

---
+++ app/config/development.php
@@ -0,0 +1,48 @@
+<?php
+
+//Konfiguracijsk niz sa najbitnijim postavkama za aplikaciju
+
+return [
+	'app' => [
+		'url' => 'http://localhost',
+		'hash' => [
+			'algo' => PASSWORD_BCRYPT,
+			'cost' => 10
+		]
+	],
+
+	'db' => [
+		'driver' => 'mysql',
+		'host' => '127.0.0.1',
+		'name' => 'church',
+		'username' => 'root',
+		'password' => 'g1o2r3a8n4',
+		'charset' => 'utf8',
+		'collation' => 'utf8_unicode_ci',
+		'prexif' => ''
+	],
+
+	'auth' => [
+		'session' => 'user_id',
+		'remember' => 'user_r'
+	],
+
+	'mail' => [
+		'smtp_auth' => true,
+		'smtp_secure' => 'tls',
+		'host' => 'milosdakic89@gmail.com',
+		'password' => 'm1I9l9O8s9',
+		'port' => 587,
+		'html' => true
+	],
+
+	'twig' => [
+		'debug' => true
+	],
+
+	'csrf' => [
+		'key' => 'csrf_token'
+	]
+];
+
+?>
---
+++ app/start.php
@@ -0,0 +1,35 @@
+<?php
+
+//Pozivanje svih namespace-ova
+use Slim\Slim;
+
+use Noodlehaus\Config;
+
+//Pokretanje sessije
+session_cache_limiter(false);
+session_start();
+
+//Ukljucivanje i prikazivanje gresaka,a u produkcijskim uslovima ovo trebamo staviti na Off
+ini_set('display_errors', 'On');
+
+//Drugo definirati include root putanju (putanja do include foldera)
+//Daje nam apsolutnu putanju do root foldera (C:\xampp\htdocs\Vijezbe\Church) "echo INC_ROOT",olaksava uljucivanje dr. fold. i fajlova
+define('INC_ROOT', dirname(__DIR__));
+
+//Ukljucivanje autoload.php fajla
+require_once INC_ROOT . '/vendor/autoload.php';
+
+
+//Nova instanca Slim klase sa opcijama u konstrktoru
+$app = new Slim([
+	'mode' => file_get_contents(INC_ROOT . '/mode.txt')
+]);
+
+//Ucitaavanje konfiguracijskih postavki iz production ili developmenet fajla,preko 'mode' u konstrktoru Slim k.
+//A koristimo configureMode() Slim m. da te konf. dodamo u Slim contaner. Coristimo Config klasu iz hasankan paketa i load() m. za povlacenje config. fajla
+
+$app->configureMode($app->config('mode'), function() use ($app) {
+	$app->config = Config::load(INC_ROOT . "/app/config/{$app->mode}.php");
+});
+
+?>
