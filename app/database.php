5df6372a49a175aec8b93709e6581d8edbb7649d
Goran R <goranradmanovic@gmail.com>
2015-08-10 12:59:11 +0200

    Correcting code in database.php file

--- app/database.php
+++ app/database.php
@@ -10,15 +10,15 @@ $capsule = new Capsule;
 //Dodavanje konkcije na bazu p. iz production.php ili development.php fajla,uz pomoc Capsule metoda addConnection() sa nizom arg. 
 
 $capsule->addConnection([
-	'driver' => $app->config->get('db.driver'),
-	'host' => $app->config->get('db.host'),
-	'name' => $app->config->get('db.name'),
-	'username' => $app->config->get('db.username'),
-	'password' => $app->config->get('db.password'),
-	'charset' => $app->config->get('db.charset'),
-	'collation' => $app->config->get('db.collation'),
-	'prefix' => $app->config->get('db.prefix')
-]);
+		'driver' => $app->config->get('db.driver'),
+		'host' => $app->config->get('db.host'),
+		'database' => $app->config->get('db.name'),
+		'username' => $app->config->get('db.username'),
+		'password' => $app->config->get('db.password'),
+		'charset' => $app->config->get('db.charset'),
+		'collation' => $app->config->get('db.collation'),
+		'prefix' => $app->config->get('db.prefix')
+	]);
 
 //Pokretanje Eloquenta da ga mozemo koristiti sa nasim models tj. modelom.
 //bootEloquent() je metod iz Capsule klase za pokretanje Eloqunta za rad sa bazom p.
