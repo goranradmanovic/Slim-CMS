6645acaeff75ad3cc58bc82dd23ebed03b664f33
Goran R <goranradmanovic@gmail.com>
2015-08-16 14:08:53 +0200

    Adding login routes to the routes.php file

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@
 //Ukljucivanje svih fajlova sa putanjama do stranica iz views foldera.Svaki novi routes file sa putanjom moramo ovdje uljuciti
 
 require_once INC_ROOT . '/app/routes/home.php';
-require_once INC_ROOT . '/app/routes/auth/register.php'
+require_once INC_ROOT . '/app/routes/auth/register.php';
+require_once INC_ROOT . '/app/routes/auth/login.php';
 
 ?>
