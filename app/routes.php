9af366f3ea4549c03b0da189e86e14f296e16772
Goran R <goranradmanovic@gmail.com>
2015-08-22 14:23:40 +0200

    Addinf path ie. routes for logout.php file

--- app/routes.php
+++ app/routes.php
@@ -6,5 +6,6 @@ require_once INC_ROOT . '/app/routes/home.php';
 require_once INC_ROOT . '/app/routes/auth/register.php';
 require_once INC_ROOT . '/app/routes/auth/login.php';
 require_once INC_ROOT . '/app/routes/auth/activate.php';
+require_once INC_ROOT . '/app/routes/auth/logout.php';
 
 ?>
