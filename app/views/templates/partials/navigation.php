8b83f3c5a7de0aae07b8fc9237b4a6457b7f40d6
Goran R <goranradmanovic@gmail.com>
2015-08-23 14:46:11 +0200

    Adding link for user profile page

--- app/views/templates/partials/navigation.php
+++ app/views/templates/partials/navigation.php
@@ -7,5 +7,6 @@
 		<li><a href="{{ urlFor('register') }}">Register</a></li>
 		<li><a href="{{ urlFor('login') }}">Login</a></li>
 		<li><a href="{{ urlFor('logout') }}">Logout</a></li>
+		<li><a href="{{ urlFor('user.profile', {username: auth.username}) }}">Your Profile</a></li>
 	</ul>
 </menu>
