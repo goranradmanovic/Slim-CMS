d1ce4d7fa145101fb5189d0066df531702a407dd
Goran R <goranradmanovic@gmail.com>
2015-08-23 19:11:52 +0200

    Restricting displying links to user in navigation

--- app/views/templates/partials/navigation.php
+++ app/views/templates/partials/navigation.php
@@ -1,12 +1,29 @@
 <!--Navigacija stranice-->
 
+{% if auth %}
+	<p>Hello, {{ auth.getFullNameOrUsername }} <img src="" alt="Your profile picture"></p>
+{% endif %}
 <menu>
 	<ul>
 		<li><a href="{{ urlFor('home') }}">Home</a></li>
 
-		<li><a href="{{ urlFor('register') }}">Register</a></li>
-		<li><a href="{{ urlFor('login') }}">Login</a></li>
-		<li><a href="{{ urlFor('logout') }}">Logout</a></li>
-		<li><a href="{{ urlFor('user.profile', {username: auth.username}) }}">Your Profile</a></li>
+		<!--Ako je korisnik ulogovan onda ne zelimo da prikazjemo link za register i login stranicu,
+			a ako je korisnik potvrdjen autentificiran onda cemo prikazati link za logout-->
+
+		{% if auth %}
+			<li><a href="{{ urlFor('logout') }}">Logout</a></li>
+
+			<!--Propustamo placeholder username: i vrijednost auth.username da bi se generisao link profilnu stranicu odredjenog korisnika-->
+			<li><a href="{{ urlFor('user.profile', {username: auth.username}) }}">Your Profile</a></li>
+
+			<!--Ako je korisnik admin prikazujemo mu ovaj link,a ako nije onda ga sakrijemo od njega-->
+			{# {% if user.isAdmin %}
+
+			{% endif %} #}
+			
+		{% else %}
+			<li><a href="{{ urlFor('register') }}">Register</a></li>
+			<li><a href="{{ urlFor('login') }}">Login</a></li>
+		{% endif %}
 	</ul>
 </menu>
