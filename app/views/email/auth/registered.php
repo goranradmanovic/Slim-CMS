c22d5c030c88c45ff212b2d3232332b4f107161c
Goran R <goranradmanovic@gmail.com>
2015-08-23 14:17:58 +0200

    Fixing minor errors

--- app/views/email/auth/registered.php
+++ app/views/email/auth/registered.php
@@ -1,9 +1,9 @@
-{% extends 'templates/default.php' %}
+{% extends 'email/templates/default.php' %}
 
 {% block content %}
 	<p>You have registerd!</p>
 
-	<p>Acctivate your account using this link: {{ baseUrl }}{{ urlFor('activate') }}?email={{ user.email }}&identifier={{ identifier|url_encode }}</p>
+	<p>Activate your account using this link: {{ baseUrl }}{{ urlFor('activate') }}?email={{ user.email }}&identifier={{ identifier|url_encode }}</p>
 {% endblock %}
 
 {# Na kraju aktivacijskog linka stavljamo tj. dodajemo query string uz pomoc ? zato sto saljemo sa GET Metodom i dodajemo
