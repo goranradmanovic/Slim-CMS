e3677ca7f15ecf46de73cee573f0251c55118038
Goran R <goranradmanovic@gmail.com>
2015-08-23 21:39:26 +0200

    Adding routes in the link Upload Profile Picture

--- app/views/templates/partials/navigation.php
+++ app/views/templates/partials/navigation.php
@@ -15,7 +15,7 @@
 
 			<!--Propustamo placeholder username: i vrijednost auth.username da bi se generisao link profilnu stranicu odredjenog korisnika-->
 			<li><a href="{{ urlFor('user.profile', {username: auth.username}) }}">Your Profile</a></li>
-			<li><a href="">Upload Profile Picture</a></li>
+			<li><a href="{{ urlFor('upload') }}">Upload Profile Picture</a></li>
 
 			<!--Ako je korisnik admin prikazujemo mu ovaj link,a ako nije onda ga sakrijemo od njega-->
 			{# {% if user.isAdmin %}
