52ec1eb9a6bea04f02206774526cf9e053f64677
Goran R <goranradmanovic@gmail.com>
2015-08-24 21:37:53 +0200

    Working on the file

--- app/routes/upload/upload_img.php
+++ app/routes/upload/upload_img.php
@@ -11,7 +11,40 @@ $app->get('/upload', function() use ($app) {
 
 $app->post('/upload', function() use ($app) {
 
-	$app->render('/upload/upload_img.php');
+	//Dohvatanje Slim request objekata
+	$request = $app->request;
+
+	$img_title = $request->post('img_title');
+	//$picture = $request->post('C:\xampp\tmp\\', function() { isset($_FILES['picture']) ?: $_FILES['picture']; });
+	$picture = $_FILES['picture'];
+
+	//Pozivanje validacijske klase
+	$v = $app->validation;
+
+	//Validacija polja iz forme
+	$v->validate([
+		'img_title' => [$img_title, 'required|alnumDash|min(4)'],
+		'picture' => [$picture, 'required']
+	]);
+	
+	//Ako je validacija prosla uspijesno
+
+	if ($v->passes())
+	{
+		echo 'ok';
+	}
+
+
+
+
+
+
+
+
+	$app->render('/upload/upload_img.php', [
+		'errors' => $v->errors(),
+		'request' => $request
+	]);
 
 })->name('upload.post');
 
