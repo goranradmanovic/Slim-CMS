9e474b36c65b62fd5e626fc319c640979491cb4a
Goran R <goranradmanovic@gmail.com>
2015-09-03 21:57:42 +0200

    Finished all.php views file and page to display to the users

---
+++ app/views/user/all.php
@@ -0,0 +1,36 @@
+{% extends 'templates/default.php' %}
+
+{% block title %}All users{% endblock %}
+
+{% block content %}
+	<h2>All users</h2>
+
+	<!--Provjera da li je korisnicki niz prazan koji se nalazi u routes/all.php falju u var. $users-->
+
+	{% if users is empty %}
+		<p>No registered users.</p>
+	{% else %}
+
+		<!--Prolaz kroz sve el. $users var koja je niz sa svim korisnicim aiz baze-->
+
+		{% for user in users %}
+			<div class="user">
+				<a href="{{ urlFor('user.profile', {username: user.username})}}">{{ user.username }}</a>
+
+				<!--Provjera da li korisnik ima puno ime i prezime i ispis ako ima-->
+
+				{% if user.getFullName %}
+					<h3>{{ user.getFullName }}</h3>
+				{% endif %}
+
+				<!--Provjera korisnikovih priviliegija-->
+
+				{% if user.isAdmin %}
+					<small>Admin</small>
+				{% endif %}
+			</div>
+		{% endfor %}
+
+	{% endif %}
+
+{% endblock %}
