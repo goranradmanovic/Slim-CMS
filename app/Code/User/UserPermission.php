6c8ee05549317cb33d17ec2d3959ffb5c90ae548
Goran R <goranradmanovic@gmail.com>
2015-09-03 22:35:10 +0200

    Finised UsersPermission class and file

---
+++ app/Code/User/UserPermission.php
@@ -0,0 +1,29 @@
+<?php
+
+//Imenovanje klase i fajla
+namespace Code\User;
+
+//Koristenje Eloquenta
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class UserPermission extends Eloquent
+{
+	//Ovo je tabele iz baze p. i ovo se zahtijea od strane Eloquenta
+
+	protected $table = 'users_permissions';
+
+	//Definiramo fillable polja u koja se moze upisivati u bazu p.
+
+	protected $fillable = [
+		'is_admin'
+	];
+
+	//Definiranje default vrijednosti kad se korisnik registruje,a mozemo staviti jos polja iz baze p. ovdje ako nam treba
+	//npr. ako zelimo da korisnik ima pravo da nesto stavi na blog ili slicno
+
+	public static $default = [
+		'is_admin' => false
+	];
+}
+
+?>
