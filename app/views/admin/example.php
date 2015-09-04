6bf750591b98fb926cb1a7a3678214d5afa88ee5
Goran R <goranradmanovic@gmail.com>
2015-09-04 23:33:31 +0200

    Finished example.php

---
+++ app/views/admin/example.php
@@ -0,0 +1,7 @@
+{% extends 'templates/default.php' %}
+
+{% block title %}Admin example{% endblock %}
+
+{% block content %}
+	<p>This is example views and this will be changed soon!!!!</p>
+{% endblock %}
