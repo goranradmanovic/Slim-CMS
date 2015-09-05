948748ddeb510b7317a020765d014e4a27ecfc92
Goran R <goranradmanovic@gmail.com>
2015-09-05 21:16:29 +0200

    Adding recover route to the file

--- app/routes.php
+++ app/routes.php
@@ -14,6 +14,7 @@ require_once INC_ROOT . '/app/routes/upload/upload_img.php';
 require_once INC_ROOT . '/app/routes/admin/example.php';
 require_once INC_ROOT . '/app/routes/errors/404.php';
 require_once INC_ROOT . '/app/routes/auth/password/change.php';
+require_once INC_ROOT . '/app/routes/auth/password/recover.php';
 
 
 ?>
