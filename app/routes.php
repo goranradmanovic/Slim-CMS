6ee4f71fc6d76ea06b96d69c51bc6357c1cd329f
Goran R <goranradmanovic@gmail.com>
2015-09-20 17:53:28 +0200

    Adding route for pdf.php

--- app/routes.php
+++ app/routes.php
@@ -17,5 +17,6 @@ require_once INC_ROOT . '/app/routes/auth/password/change.php';
 require_once INC_ROOT . '/app/routes/auth/password/recover.php';
 require_once INC_ROOT . '/app/routes/auth/password/reset.php';
 require_once INC_ROOT . '/app/routes/account/profile.php';
+require_once INC_ROOT . '/app/routes/assets/pdf.php';
 
 ?>
