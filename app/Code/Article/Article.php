9b855d5ffd88ddcf504a56cb2bd278103f802672
Goran R <goranradmanovic@gmail.com>
2015-09-25 22:25:01 +0200

    Working on Article class

--- app/Code/Article/Article.php
+++ app/Code/Article/Article.php
@@ -14,8 +14,40 @@ class Article extends Eloquent
 	//Definisanje kolona u koje je dozvoljen upis u bazu p.
 	protected $fillable = [
 		'user_id',
+		'title',
 		'text'
 	];
+
+	//Funkcija za dohvatanje nsalova clanka
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	//Funkcija za dohvatanje sadrzaja clanka
+
+	public function getText()
+	{
+		return $this->text;
+	}
+
+	//Funcija za editovanje clanka
+
+	public function editArticle($title, $text)
+	{
+		$this->update([
+			'title' => $title,
+			'text' => $text
+		]);
+	}
+
+	//Funkcija za brisanje clanka
+
+	public function deleteArticle($id, $user_id)
+	{
+		$this->where('id', $id)->where('user_id', $user_id)->delete();
+	}
 }
 
 ?>
