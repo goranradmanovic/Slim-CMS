09c6223c9a38543086490bb7c9b4123a6790e94d
Goran R <goranradmanovic@gmail.com>
2015-09-25 21:34:41 +0200

    Finishing article input form

---
+++ app/views/articles/article.php
@@ -0,0 +1,24 @@
+{% extends 'templates/default.php' %}
+
+{% block title %}Article{% endblock %}
+
+{% block content %}
+	<form action="{{ urlFor('article.post') }}" method="post" autocomplete="off">
+		<div>
+			<label for="title">Title</label>
+			<input type="text" name="title" id="title" {% ifrequest.post('title') %} value="{{ request.post('title') }}" {% endif %}>
+			{% if errors.has('title') %} {{ errors.first('title') }} {% endif %}
+		</div>
+
+		<div>
+			<label for="article">Article</label>
+			<textarea name="article" id="article"></textarea>
+			{% if errors.has('article') %} {{ errors.first('article') }} {% endif %}
+		</div>
+
+		<div>
+			<input type="submit" value="Publish">
+			<input type="hidden" name="{{ csrf_key }}" value="{{ csrf_token }}">
+		</div>
+	</form>
+{% endblock %}
