fea7d2d727848313f7a7cdaf26ab0126d6a507c3
Goran R <goranradmanovic@gmail.com>
2015-09-27 22:03:42 +0200

    Adding route for all_posts.php

--- app/routes.php
+++ app/routes.php
@@ -20,6 +20,7 @@ require_once INC_ROOT . '/app/routes/account/profile.php';
 require_once INC_ROOT . '/app/routes/assets/pdf.php';
 require_once INC_ROOT . '/app/routes/assets/xlsx.php';
 require_once INC_ROOT . '/app/routes/articles/article.php';
+require_once INC_ROOT . '/app/routes/post/all_posts.php';
 require_once INC_ROOT . '/app/routes/post/show.php';
 
 ?>
