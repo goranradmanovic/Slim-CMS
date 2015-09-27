17596299b8fd1af977ed0885011ca9cee4692ce1
Goran R <goranradmanovic@gmail.com>
2015-09-27 22:07:36 +0200

    Finished show.php views

--- app/routes/post/show.php
+++ app/routes/post/show.php
@@ -4,13 +4,16 @@
 //Propsutanje var. kojoj mozemo pistupiti i vidjti koji post iz baze zelimo prikazati korisniku,i koju dohvatamo
 //uz pomoc URL-a i GET-a,prvi param je putanja,onda tu var. postId propustamo u closure funck kao param
 
-$app->get('/posts/:postId', function($postId) use ($app) {
+$app->get('/posts/:postId', $guest(), function($postId) use ($app) {
 
 	//Dohvatanje postova iz baze sa specificnim id-em npr. (1,2,3) kojeg dobijamo kroz URL
 	//:postId je PDO placeholder koji poslije zamjenimo sa stvarnom vrijednoscu i nema veza sa :postId iz URL-a
 
-	$post = $app->article->query("SELECT articles.*, users.username, users.first_name, users.last_name FROM articles LEFT JOIN users
-									ON articles.user_id = users.id WHERE articles.id = postId");
+	//$post = $app->article->query('SELECT articles.*, users.username, users.first_name, users.last_name FROM articles LEFT JOIN users
+									//ON articles.user_id = users.id WHERE articles.id = postId')->get();
+
+	$post = $app->article->leftJoin('users', 'articles.user_id', '=', 'users.id')->select('articles.*','users.username','users.first_name','users.last_name')
+	->where('articles.id', '=', $postId)->get();
 
 	//Ako post nije pronadjen tj. ako nemamo nista u bazi,onda cemo output 404 gresku
 
