e5027347c62097dda929676d45e4d7faefbd8a11
Goran R <goranradmanovic@gmail.com>
2015-09-27 18:46:11 +0200

    Finished show.php views for displaying single post from user

---
+++ app/views/posts/show.php
@@ -0,0 +1,16 @@
+{% extends 'templates/default.php' %}
+
+{% block title %} Post {% endblock%}
+
+{% block content %}
+	<a href="{{ urlFor('home') }}">Back</a>
+
+	<h2>{{ post.title }}</h2>
+
+	<p>{{ post.text }}</p>
+
+	<div class="author">
+		<p>By {{ post.getUsernameOrFullName() }} on post.created.</p>
+	</div>
+	
+{% endblock %}
