6757389d8c4cba1a2f07b2b6c57ca083cd0ae3a3
Goran R <goranradmanovic@gmail.com>
2015-09-27 22:08:57 +0200

    Finished all_posts.php route file

---
+++ app/routes/post/all_posts.php
@@ -0,0 +1,15 @@
+<?php
+
+//Get putanja do fajla
+
+$app->get('/posts/all_posts', $guest(), function() use ($app) {
+
+	//$posts = $app->article->query("SELECT articles.*, CONCAT(users.first_name, ' ', users.last_name) AS author FROM articles LEFT JOIN users ON articles.user_id = users.id")->get();
+
+	$posts = $app->article->leftJoin('users', 'articles.user_id', '=', 'users.id')->select('articles.*','users.username','users.first_name','users.last_name')->orderBy('articles.id')->get();
+
+	$app->render('posts/all_posts.php', ['posts' => $posts]);
+
+})->name('posts.all_posts');
+
+?>
