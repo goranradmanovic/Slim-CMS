b24c406cd19abe16e2c9b0969c3f8ae17ed1e1db
Goran R <goranradmanovic@gmail.com>
2015-09-28 13:29:52 +0200

    Adding edit article link

--- app/views/templates/partials/navigation.php
+++ app/views/templates/partials/navigation.php
@@ -20,6 +20,7 @@
 			<li><a href="{{ urlFor('password.change') }}">Change password</a></li>
 			<li><a href="{{ urlFor('account.profile') }}">Update profile</a></li>
 			<li><a href="{{ urlFor('articles.article') }}">Publish Article</a></li>
+			<li><a href="{{ urlFor('articles.edit') }}">Edit Article</a></li>
 
 			<!--Ako je korisnik admin prikazujemo mu ovaj link,a ako nije onda ga sakrijemo od njega-->
 			{% if auth.isAdmin %}
