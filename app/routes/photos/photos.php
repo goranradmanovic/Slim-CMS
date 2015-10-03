20e571d6084abc3d27a502986f97a1f720c868a3
Goran R <goranradmanovic@gmail.com>
2015-10-03 21:14:49 +0200

    Creating photos folder for photo route

---
+++ app/routes/photos/photos.php
@@ -0,0 +1,13 @@
+<?php
+
+//Get putanja
+
+$app->get('/photos', $authenticated(), function () use ($app) {
+
+	//Dohvatanje photos st. iz views/photos foldera
+	return $app->render('/photos/photos.php');
+
+})->name('photos');
+
+
+?>
