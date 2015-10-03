0eaa18992db73e3422a1a56d7066b224014bcfc9
Goran R <goranradmanovic@gmail.com>
2015-10-03 21:15:56 +0200

    Adding markup to gallery file

--- app/views/gallery/gallery.php
+++ app/views/gallery/gallery.php
@@ -1,1 +1,13 @@
-hello
+{% extends 'templates/default.php' %}
+
+{% block title %} Gallery {% endblock %}
+
+{% block content %}
+	
+	<div class="gallery">
+		<div class="albums">
+			Hello
+		</div>
+	</div>
+
+{% endblock %}
