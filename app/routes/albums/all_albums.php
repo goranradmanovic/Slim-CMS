dd6387789920b1588e7347b3f729562706b6d12b
Goran R <goranradmanovic@gmail.com>
2015-10-04 14:17:08 +0200

    Testing some functionallity

---
+++ app/routes/albums/all_albums.php
@@ -0,0 +1,18 @@
+<?php
+
+//Get putanja
+
+$app->get('/all_albums', $authenticated(), function () use ($app) {
+
+	//Dohvatanje svih albuma od specificnog korisnika
+
+	$albums = $app->album->getUserAlbums($app->auth->id);
+
+	//Dohvatanje stranice iz viewsa i slanje podataka
+	return $app->render('/albums/all_albums.php', [
+		'albums' => $albums
+	]);
+
+})->name('ablums.all_albums');
+
+?>
