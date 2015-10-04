b667e8052cc7ee3d7a14d67577b08f5b97a01ed9
Goran R <goranradmanovic@gmail.com>
2015-10-04 21:30:09 +0200

    Adding route for upload_photos.php

--- app/routes.php
+++ app/routes.php
@@ -11,6 +11,7 @@ require_once INC_ROOT . '/app/routes/user/profile.php';
 require_once INC_ROOT . '/app/routes/user/delete_img.php';
 require_once INC_ROOT . '/app/routes/user/all.php';
 require_once INC_ROOT . '/app/routes/upload/upload_img.php';
+require_once INC_ROOT . '/app/routes/upload/upload_photos.php';
 require_once INC_ROOT . '/app/routes/admin/example.php';
 require_once INC_ROOT . '/app/routes/errors/404.php';
 require_once INC_ROOT . '/app/routes/auth/password/change.php';
