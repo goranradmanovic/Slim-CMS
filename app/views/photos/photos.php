e9642c9637b96bb2aa3d04b444c55d1e443217b4
Goran R <goranradmanovic@gmail.com>
2015-10-04 14:12:35 +0200

    Fixing bas url from the link

--- app/views/photos/photos.php
+++ app/views/photos/photos.php
@@ -7,14 +7,14 @@
 	<div class="photos">
 		
 		<ul>
-			<li><a href="{{ urlFor('photos.create_album') }}">+ Create Album</a></li>
+			<li><a href="{{ urlFor('albums.create_album') }}">+ Create Album</a></li>
 			<li><a href="#">Add Photos</a></li>
 		</ul>
 
 		<div class="">
 			<ul>
 				<li><a href="#">Your Photos</a></li>
-				<li><a href="#">Albums</a></li>
+				<li><a href="{{ urlFor('ablums.all_albums') }}">Albums</a></li>
 			</ul>
 		</div>
 	</div>
