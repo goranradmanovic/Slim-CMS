3b816d16c782a6e8832f32ef596c02e8c1ea66d6
Goran R <goranradmanovic@gmail.com>
2015-10-06 23:10:33 +0200

    Adding required attr. to the form fields

--- app/views/upload/upload_photos.php
+++ app/views/upload/upload_photos.php
@@ -7,26 +7,26 @@
 	<form action="{{ urlFor('upload.photos.post') }}" method="post" enctype="multipart/form-data">
 		<div>
 			<label for="album">Select Album</label>
-			<select name="albums" id="album">
-				<option>---</option>
+			<select name="albums" id="album" required>
+				<option value="">None</option>
 
 				{% for album in albums %}
 					<option value="{{ album.id }}">{{ album.title }}</option>
 				{% endfor %}
 			</select>
-			{% if errors.has('album') %} {{ errors.first('album') }} {% endif %}
+			{% if errors.has('albums') %} {{ errors.first('albums') }} {% endif %}
 		</div>
 
 		<div>
 			<label for="photos_upload">Upload Photos</label>
-			<input type="file" name="photos[]" multiple="multiple" id="photos_upload">
-			{% if errors.has('photos') %} {{ errors.first('photos') }} {% endif %}
+			<input type="file" name="photos[]" multiple required id="photos_upload">
+			{% if errors.has('photos.name') %} {{ errors.first('photos.name') }} {% endif %}
 		</div>
 
 		<div>
 			<input type="submit" value="Upload Photos">
 			<input type="hidden" name="{{ csrf_key }}" value="{{ csrf_token }}">
-			<input type="hidden" name="MAX_FILE_SIZE" value="30000" /> <!--Ogranicenje velicine fajla za slanje-->
+			<input type="hidden" name="MAX_FILE_SIZE" value="5242880" /> <!--Ogranicenje velicine fajla za slanje-->
 		</div>
 	</form>
 
