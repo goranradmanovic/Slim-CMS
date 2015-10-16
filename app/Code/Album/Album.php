57f588b875031a098ea54d70baeb6e0d3538b028
Goran R <goranradmanovic@gmail.com>
2015-10-16 22:44:57 +0200

    Working on the function getAlbumThumbnail()

--- app/Code/Album/Album.php
+++ app/Code/Album/Album.php
@@ -17,17 +17,34 @@ class Album extends Eloquent
 	protected $fillable = ['user_id', 'title'];
 
 	//Metod za dohvatanje svih albuma iz baze p.
-
 	public function getAlbums()
 	{
 		return $this->select('*')->get();
 	}
 
 	//Metod za dohvatanje imena albuma od specificnog korisnika
-
 	public function getUserAlbums($user_id)
 	{
 		return $this->where('user_id', $user_id)->get();
+	}
+
+	//Povezivanje photo tabele sa album tabelom
+	public function photo()
+	{
+		return $this->hasOne('Code\Photo\Photo');
+	}
+
+	//Metod za brojeanje slika u odredjenom albumu
+	public function countPhotosInAlbum()
+	{
+		return $this->photo->select('id')->where('album_id', $this->id)->count();
+	}
+
+	//Metod za dohvatanje prve slike u Albumu
+	public function getAlbumThumbnail()
+	{
+		//Dohvatanje prve slike iz albuma tj. njene putanje
+		return $this->photo->where('album_id', $this->id)->first(['path']);
 	}
 
 }
