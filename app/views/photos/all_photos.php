871abb7ab80e4b4f5cd8cf99b1bca679ab6eaab2
Goran R <goranradmanovic@gmail.com>
2015-10-19 23:07:48 +0200

    Finished displying all user photos

---
+++ app/views/photos/all_photos.php
@@ -0,0 +1,15 @@
+{% extends 'templates/default.php' %}
+
+{% block title %}All Photos{% endblock %}
+
+{% block content %}
+	
+	<div class="all-photos">
+		<div class="photos">
+			{% for photo in photos %}
+				<a href="{{ urlFor('photos.photo') }}?id={{ photo.id }}"><img src="{{ photo.path }}" alt="User photo.jpg" width="300px" height="300px"></a>
+			{% endfor %}
+		</div>
+	</div>
+
+{% endblock %}
