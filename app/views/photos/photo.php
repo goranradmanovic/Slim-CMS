6b9e75e3436732eb7e126848078032c5c340217f
Goran R <goranradmanovic@gmail.com>
2015-10-19 23:08:17 +0200

    Finishing di

---
+++ app/views/photos/photo.php
@@ -0,0 +1,14 @@
+{% extends 'templates/default.php' %}
+
+{% block title %}All Photos{% endblock %}
+
+{% block content %}
+	
+	<div class="single-photo">
+		<div class="photo">
+				<a href="{{ urlFor('photos.all_photos') }}">Back</a>
+				<img src="{{ photo.path }}" alt="User photo.jpg" width="75%" height="75%">
+		</div>
+	</div>
+
+{% endblock %}
