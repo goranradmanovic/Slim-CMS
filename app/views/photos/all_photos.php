ff374ad25e58104c31140723a5ef9bb8cfdbb249
Goran R <goranradmanovic@gmail.com>
2015-10-24 22:09:50 +0200

    Adding link for delete photo

--- app/views/photos/all_photos.php
+++ app/views/photos/all_photos.php
@@ -8,6 +8,7 @@
 		<div class="photos">
 			{% for photo in photos %}
 				<a href="{{ urlFor('photos.photo') }}?id={{ photo.id }}"><img src="{{ photo.path }}" alt="User photo.jpg" width="300px" height="300px"></a>
+				<a href="{{ urlFor('delete_photo') }}?id={{ photo.id }}">Delete photo</a>
 			{% endfor %}
 		</div>
 	</div>
