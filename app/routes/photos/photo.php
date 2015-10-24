bdf1f601538147b782dc7fa2ac1a2083dd255e4e
Goran R <goranradmanovic@gmail.com>
2015-10-24 22:00:44 +0200

    Adding request object to the file

--- app/routes/photos/photo.php
+++ app/routes/photos/photo.php
@@ -4,18 +4,21 @@
 
 $app->get('/photo', function () use ($app) {
 
+	//Request objekat
+	$request = $app->request;
+
 	//Dohvatanje Photo klase iz Slim cont.
 	$photo = $app->photo;
 
+	//Dohvatanje id od sliek
+	(int) $photoId = $request->get('id');
+
 	//Provjera da li je id poslat krou URL i da li je id prazan
-	if (!isset($_GET['id']) || empty($_GET['id']))
+	if (!isset($photoId) || empty($photoId))
 	{	
 		//Redirekcija u slucaju da id od slike ne postoji
 		return $app->redirect($app->urlFor('photos.all_photos'));
 	}
-
-	//Dohvatanje id od sliek
-	(int) $photoId = $_GET['id'];
 
 	//Povlacenje spscificne slike iz baze p.
 	$OnePhoto = $photo->getPhoto($photoId);
