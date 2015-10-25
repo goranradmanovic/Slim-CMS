30298467277edfa1171e27a2fdb94523b99bd089
Goran R <goranradmanovic@gmail.com>
2015-10-25 21:23:30 +0100

    Finished album_photos views

--- app/views/albums/album_photos.php
+++ app/views/albums/album_photos.php
@@ -7,12 +7,13 @@
 	<div class="album-photos">
 		
 		{% if albumPhotos is null %}
-				<p>There's no photos in this album.</p>
+			<p>There's no photos in this album.</p>
 		{% else %}
 			{% for albumPhoto in albumPhotos %}
-				<img src="{{ albumPhoto.path  }}" alt="Album Photo.jpg" width="200px" height="200px">
+				<a href="{{ urlFor('photos.photo') }}?id={{ albumPhoto.id }}"><img src="{{ albumPhoto.path  }}" alt="Album Photo.jpg" width="206px" height="206px"></a>
 			{% endfor %}
 		{% endif %}
+
 		<a href="{{ urlFor('albums.all_albums') }}">Go back</a>
 	</div>
 
