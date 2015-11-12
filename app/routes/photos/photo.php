bbe701fd02df9e06af811d9faf9e7de69839e5d2
Goran R <goranradmanovic@gmail.com>
2015-11-12 21:16:10 +0100

    Adding :id placeholder to the route URL

--- app/routes/photos/photo.php
+++ app/routes/photos/photo.php
@@ -1,27 +1,13 @@
 <?php
 
 //Get putanja
-
-$app->get('/photo', function () use ($app) {
-
-	//Request objekat
-	$request = $app->request;
+$app->get('/photo/:id', function($id) use ($app) {
 
 	//Dohvatanje Photo klase iz Slim cont.
 	$photo = $app->photo;
 
-	//Dohvatanje id od sliek
-	(int) $photoId = $request->get('id');
-
-	//Provjera da li je id poslat krou URL i da li je id prazan
-	if (!isset($photoId) || empty($photoId))
-	{	
-		//Redirekcija u slucaju da id od slike ne postoji
-		return $app->redirect($app->urlFor('photos.all_photos'));
-	}
-
 	//Povlacenje spscificne slike iz baze p.
-	$OnePhoto = $photo->getPhoto($photoId);
+	$OnePhoto = $photo->getPhoto($id);
 
 	//Vracanje view-a korisniku sa odredjenom slikom
 	return $app->render('/photos/photo.php', [
