bf5b4c99d52e0d84e588bed804867495b34bb2f4
Goran R <goranradmanovic@gmail.com>
2015-11-12 21:23:09 +0100

    Adding :id placeholder to the views URL

--- app/views/photos/all_photos.php
+++ app/views/photos/all_photos.php
@@ -7,8 +7,8 @@
 	<div class="all-photos">
 		<div class="photos">
 			{% for photo in photos %}
-				<a href="{{ urlFor('photos.photo') }}?id={{ photo.id }}"><img src="{{ photo.path }}" alt="User photo.jpg" width="300px" height="300px"></a>
-				<a href="{{ urlFor('delete_photo') }}?id={{ photo.id }}">Delete photo</a>
+				<a href="{{ urlFor('photos.photo', {'id': photo.id}) }}"><img src="{{ photo.path }}" alt="User photo.jpg" width="300px" height="300px"></a>
+				<a href="{{ urlFor('delete_photo', {'id': photo.id}) }}">Delete photo</a>
 			{% endfor %}
 		</div>
 	</div>
