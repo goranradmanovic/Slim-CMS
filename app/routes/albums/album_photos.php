512ec298f538feae1b3b7509e29bef678fe48eb4
Goran R <goranradmanovic@gmail.com>
2015-11-12 21:06:27 +0100

    Adding :id placeholder to route URL

--- app/routes/albums/album_photos.php
+++ app/routes/albums/album_photos.php
@@ -1,26 +1,20 @@
 <?php
 
 //Get putanja do fajla
-$app->get('/album_photos', function () use ($app) {
-
-	//Request objekat
-	$request = $app->request;
+$app->get('/album_photos/:id', function ($id) use ($app) {
 
 	//Album objekta
 	$album = $app->album;
 
-	//Dohvatanje ID od albuma iz URL-a
-	(int) $albumId = $request->get('id');
-
 	//Provjera da li u GET-u postoji ID od albuma
-	if (!isset($albumId))
+	if (!isset($id))
 	{	
 		//Redirekcija korisnika na st. sa svim albumima
 		return $app->redirect($app->urlFor('albums.all_albums'));
 	}
 
 	//Metod za prikazivanje svih slika iz odredjenog albuma
-	$albumPhotos = $album->DisplayAlbumPhotos($albumId);
+	$albumPhotos = $album->DisplayAlbumPhotos($id);
 
 	//Slanje podataka na view od svih slika iz albuma
 	return $app->render('/albums/album_photos.php', [
