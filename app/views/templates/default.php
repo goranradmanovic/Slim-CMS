9b311856c6136082fe8754881647500f30a088fe
Goran R <goranradmanovic@gmail.com>
2015-11-13 12:52:42 +0100

    Adding baseURl and public route to the js script src

--- app/views/templates/default.php
+++ app/views/templates/default.php
@@ -3,15 +3,15 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="CMS, MVC, Slim2 application">
+		<meta name="description" content="This is blog cms system">
 		<meta name="author" content="Goran Radmanovic">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="apple-moblie-web-app-capable" content="yes">
-		<meta name="robots" content="index, follow, all"/>
-		<meta name="keywords" content="domajax, ajax, jquery, plugin, javascript"/>
-		<link rel="stylesheet" href="assets/css/sweetalert.css">
+		<meta name="robots" content="index, follow, all">
+		<meta name="keywords" content="blog, cms, photos, articles">
+		<link rel="stylesheet" href="{{ baseUrl }}{{ public }}assets/css/sweetalert.css">
 		<title>Website | {% block title %}{% endblock %}</title>
 	</head>
 	<body>
@@ -25,9 +25,9 @@
 		{% block content %}{% endblock %}
 
 		<script defer src='https://www.google.com/recaptcha/api.js'></script>
-		<script defer src='assets/js/zepto-min.js'></script>
-		<script src='assets/js/sweetalert.min.js'></script>
-		<script defer src='assets/js/main.js'></script>
+		<script defer src='{{ baseUrl }}{{ public }}assets/js/zepto-min.js'></script>
+		<script src='{{ baseUrl }}{{ public }}assets/js/sweetalert.min.js'></script>
+		<script defer src='{{ baseUrl }}{{ public }}assets/js/main.js'></script>
 		<script>
 			{% if flash['global'] %}
 				swal({
