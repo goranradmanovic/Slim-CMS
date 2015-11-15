a67a533a880bb2353def66911c5f428aab2a778f
Goran R <goranradmanovic@gmail.com>
2015-11-15 19:09:58 +0100

    Creating photos navigation menu

---
+++ app/views/photos/templates/partials/photos_navigation.php
@@ -0,0 +1,14 @@
+<div class="photos-nav">
+	<div class="">
+		<ul>
+			<li><a href="{{ urlFor('albums.create_album') }}">+ Create Album</a></li>
+			<li><a href="{{ urlFor('upload.photos') }}">Add Photos</a></li>
+		</ul>
+	</div>
+	<div class="">
+		<ul>
+			<li><a href="{{ urlFor('photos.all_photos') }}">Your Photos</a></li>
+			<li><a href="{{ urlFor('albums.all_albums') }}">Albums</a></li>
+		</ul>
+	</div>
+</div>
