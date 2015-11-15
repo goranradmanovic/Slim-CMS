fad4a3c4ec023c141b797c6839bff5c20f7fd7bc
Goran R <goranradmanovic@gmail.com>
2015-11-15 16:30:18 +0100

    Formating and adding defer atrribute to the script tag

--- app/views/templates/default.php
+++ app/views/templates/default.php
@@ -4,11 +4,11 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="apple-moblie-web-app-capable" content="yes">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="This is blog cms system">
 		<meta name="author" content="Goran Radmanovic">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="apple-moblie-web-app-capable" content="yes">
 		<meta name="robots" content="index, follow, all">
 		<meta name="keywords" content="blog, cms, photos, articles">
 		<link rel="stylesheet" href="{{ baseUrl }}{{ public }}assets/css/sweetalert.css">
