3859b13b40c080aea4a89aa6eb171c82650a400c
Goran R <goranradmanovic@gmail.com>
2015-11-15 16:28:42 +0100

    Formating file

--- app/views/photos/photo.php
+++ app/views/photos/photo.php
@@ -6,8 +6,8 @@
 	
 	<div class="single-photo">
 		<div class="photo">
-				<a href="{{ urlFor('photos.all_photos') }}">Back</a>
-				<img src="{{ photo.path }}" alt="User photo.jpg" width="75%" height="75%">
+			<a href="{{ urlFor('photos.all_photos') }}">Back</a>
+			<img src="{{ photo.path }}" alt="User photo.jpg" width="75%" height="75%">
 		</div>
 	</div>
 
