b588b0cf03a393006e8eb4865e1e3ef1457417e1
Goran R <goranradmanovic@gmail.com>
2015-11-16 20:02:54 +0100

    Addding check for albums if they exists

--- app/views/albums/all_albums.php
+++ app/views/albums/all_albums.php
@@ -9,29 +9,34 @@
 		<!--Ukljucivanje navigacije za opcije za upload slika i kreiranje albuma-->
 		{% include 'photos/templates/partials/photos_navigation.php' %}
 
-		{% for album in albums %}
+		<!--Provjera da li imamo neki album-->
+		{% if albums is empty %}
+			<p>You do not have a single album.</p>
+		{% else %}
+			{% for album in albums %}
 
-			<div class="album">
-				<div>
-					<a href="{{ urlFor('albums.album_photos', {'id': album.id}) }}">
-					{% if album.getAlbumThumbnail.path is null %}
-						<img src="{{ baseUrl }}{{ public }}assets/icons/Album.svg" alt="Album photos" width="200px" height="200px"/></a>
-					{% else %}
-						<img src="{{ album.getAlbumThumbnail.path }}" alt="Album photos" width="200px" height="200px"/></a>
-					{% endif %}
+				<div class="album">
+					<div>
+						<a href="{{ urlFor('albums.album_photos', {'id': album.id}) }}">
+						{% if album.getAlbumThumbnail.path is null %}
+							<img src="{{ baseUrl }}{{ public }}assets/icons/Album.svg" alt="Album photos" width="200px" height="200px"/></a>
+						{% else %}
+							<img src="{{ album.getAlbumThumbnail.path }}" alt="Album photos" width="200px" height="200px"/></a>
+						{% endif %}
+					</div>
+
+					<div>
+						<a href="{{ urlFor('albums.album_photos', {'id': album.id}) }}">{{ album.title }}</a>
+
+						<p>{{ album.countPhotosInAlbum }} photos</p>
+
+						{% if auth %}
+							<a href="#" id="btnDelete" data-identity="{{ album.id }}">Delete Album</a>
+						{% endif %}
+					</div>
 				</div>
-
-				<div>
-					<a href="{{ urlFor('albums.album_photos', {'id': album.id}) }}">{{ album.title }}</a>
-
-					<p>{{ album.countPhotosInAlbum }} photos</p>
-
-					{% if auth %}
-						<a href="#" id="btnDelete" data-identity="{{ album.id }}">Delete Album</a>
-					{% endif %}
-				</div>
-			</div>
-			<hr>
-		{% endfor %}
+				<hr>
+			{% endfor %}
+		{% endif %}
 	</div>
 {% endblock %}
