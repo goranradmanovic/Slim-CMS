354d797d2494708a17c685b51948677207f03b5b
Goran R <goranradmanovic@gmail.com>
2015-11-16 20:03:34 +0100

    Addding check for photos if they exists

--- app/views/photos/all_photos.php
+++ app/views/photos/all_photos.php
@@ -4,13 +4,21 @@
 
 {% block content %}
 	
+	<!--Ukljucivanje navigacija za slike-->
+	{% include 'photos/templates/partials/photos_navigation.php' %}
+
 	<div class="all-photos">
-		<div class="photos">
-			{% for photo in photos %}
-				<a href="{{ urlFor('photos.photo', {'id': photo.id}) }}"><img src="{{ photo.path }}" alt="User photo.jpg" width="300px" height="300px"></a>
-				<a href="{{ urlFor('delete_photo', {'id': photo.id}) }}">Delete photo</a>
-			{% endfor %}
-		</div>
+		<!--Provjera da li imamo neke slike-->
+		{% if photos is empty %}
+			<p>You do not have a single photo.</p>
+		{% else %}
+			<div class="photos">
+				{% for photo in photos %}
+					<a href="{{ urlFor('photos.photo', {'id': photo.id}) }}"><img src="{{ photo.path }}" alt="User photo.jpg" width="300px" height="300px"></a>
+					<a href="{{ urlFor('delete_photo', {'id': photo.id}) }}">Delete photo</a>
+				{% endfor %}
+			</div>
+		{% endif %}
 	</div>
 
 {% endblock %}
