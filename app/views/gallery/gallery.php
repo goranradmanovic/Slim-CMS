02a5165dc8062d3f1654f6a26efbedfebd46548f
Goran R <goranradmanovic@gmail.com>
2015-11-24 21:51:05 +0100

    Listing all data about album to the gallery.php views

--- app/views/gallery/gallery.php
+++ app/views/gallery/gallery.php
@@ -5,9 +5,21 @@
 {% block content %}
 	
 	<div class="gallery">
-		<div class="albums">
-			Hello
-		</div>
+		<!--<div id="lightgallery">
+			<a href="img/img1.jpg"><img src="img/thumb1.jpg" /></a>
+			<a href="img/img2.jpg"><img src="img/thumb2.jpg" /></a>
+		</div>-->
+		<h1>Albums</h1>
+		{% for album in albums %}
+			<div>
+				<h3>{{ album.title }}</h3>
+				<a href="{{ urlFor('albums.album_photos', {'id': album.id}) }}">
+					<img src="{{ album.getAlbumThumbnail.path }}" alt="Album thumbnail.jpg" width="250px" height="250px">
+				</a>
+				<p>Photos in album: {{ album.countPhotosInAlbum }}</p>	
+			</div>
+					
+		{% endfor %}
 	</div>
 
 {% endblock %}
