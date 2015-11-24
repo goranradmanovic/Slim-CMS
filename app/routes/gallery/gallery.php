48e9ed0fff8bb83e4523468512218b811cbc0ae7
Goran R <goranradmanovic@gmail.com>
2015-11-24 21:49:23 +0100

    Geting all albums and sending to the gallery.php views

--- app/routes/gallery/gallery.php
+++ app/routes/gallery/gallery.php
@@ -4,9 +4,12 @@
 
 $app->get('/gallery', function () use ($app) {
 
-	return $app->render('/gallery/gallery.php');
+	//Dohvatanje svih albuma iz baze p.
+	$albums = $app->album->getAlbums();
+
+	//Slanje svih albuma na gallery.php i rendovanje st.
+	return $app->render('/gallery/gallery.php', ['albums' => $albums]);
 
 })->name('gallery');
-
 
 ?>
